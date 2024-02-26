dc466bc1903b28b9f14953ba7fbe4229b644bef8
Rutunj Parikh <62198018+rutunjp@users.noreply.github.com>
2024-02-26 12:23:56 +0530

    feat: add tasks.js

---
+++ tasks.js
@@ -0,0 +1,14 @@
+export const tasks = [
+  {
+    id: 1,
+    text: "This is the first task",
+  },
+  {
+    id: 2,
+    text: "This is the second task",
+  },
+  {
+    id: 3,
+    text: "This is the third task",
+  },
+];
